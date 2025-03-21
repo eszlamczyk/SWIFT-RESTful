8e9cb22bf03e81be421059fd9ce0e70022c432f9
Ernest Szlamczyk <ernest.szlamczyk@gmail.com>
2025-03-21 17:06:43 +0100

    created database import from .csv file

---
+++ SWIFT/src/main/java/p/ernest/swift/config/DatabaseConfiguration.java
@@ -0,0 +1,94 @@
+package p.ernest.swift.config;
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import p.ernest.swift.service.BankService;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${bank.csv.filePath}")
+    private String filePath;
+
+
+    @Bean
+    CommandLineRunner initData(
+            BankService bankService
+    ){
+        return args -> {
+            if (bankService.getAmountOfBanks() < 1){
+                try {
+                    FileReader fileReader = new FileReader(new ClassPathResource(filePath).getFile());
+                    CSVReader reader = new CSVReader(fileReader);
+
+                    reader.skip(1);
+
+                    List<String[]> records = reader.readAll();
+                    List<String[]> deferredRecords = new ArrayList<>();
+
+                    for (String[] record : records) {
+                        if (record.length == 8) {
+                            String countryIso2Code = record[0];
+                            String swiftCode = record[1];
+                            String codeType = record[2];
+                            String name = record[3];
+                            String address = record[4];
+                            String townName = record[5];
+                            String countryName = record[6];
+                            String timeZone = record[7];
+
+                            if (swiftCode.endsWith("XXX")) {
+                                bankService.addNewBank(
+                                        swiftCode,
+                                        countryIso2Code,
+                                        codeType,
+                                        name,
+                                        address,
+                                        townName,
+                                        countryName,
+                                        timeZone);
+                            } else {
+                                deferredRecords.add(record);
+                            }
+                        } else {
+                            System.out.println("Invalid record format: " + String.join(",", record));
+                        }
+                    }
+                    for (String[] deferredRecord : deferredRecords) {
+                        String countryIso2Code = deferredRecord[0];
+                        String swiftCode = deferredRecord[1];
+                        String codeType = deferredRecord[2];
+                        String name = deferredRecord[3];
+                        String address = deferredRecord[4];
+                        String townName = deferredRecord[5];
+                        String countryName = deferredRecord[6];
+                        String timeZone = deferredRecord[7];
+
+                        bankService.addNewBank(
+                                swiftCode,
+                                countryIso2Code,
+                                codeType,
+                                name,
+                                address,
+                                townName,
+                                countryName,
+                                timeZone);
+                    }
+                } catch (IOException | CsvException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+}
